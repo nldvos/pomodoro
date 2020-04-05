5809c1a3277fc39a48ba15467c7a464488a069ca
Nicolas Devos <nldvos@gmail.com>
2020-04-05 18:14:26 +0200

    set up build system with hello world programs

---
+++ client.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "client.h"
+
+
+int main(int argc, char *argv[])
+{
+	printf("hello client\n");
+	return EXIT_SUCCESS;
+}
+
---
+++ server.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "server.h"
+
+
+int main(int argc, char *argv[])
+{
+	printf("hello server\n");
+	return EXIT_SUCCESS;
+}
