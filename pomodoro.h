e7ab749046e8e7180924971e724624803b6eccc2
Nicolas Devos <nldvos@gmail.com>
2020-04-13 01:08:26 +0200

    add libpomodoro functions

--- pomodoro.h
+++ pomodoro.h
@@ -5,3 +5,16 @@
 #define POMODORO_LARGE_BREAK (15)
 #define POMODORO_WORK_TIME (25)
 #define POMODORO_CYCLES (4)
+
+typedef POMODORO_HDL void*;
+
+POMODORO_HDL pomodoro_create();
+extern void pomodoro_destroy(POMODORO_HDL hdl);
+extern int pomodoro_connect(POMODORO_HDL hdl);
+
+extern int pomodoro_start(POMODORO_HDL hdl);
+extern int pomodoro_stop(POMODORO_HDL hdl);
+
+extern int pomodoro_continue(POMODORO_HDL hdl);
+extern int pomodoro_pause(POMODORO_HDL hdl);
+extern int pomodoro_skip(POMODORO_HDL hdl);
